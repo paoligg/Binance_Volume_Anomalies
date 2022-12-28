96be0c8cb094697998126f455209c8e1d04477c6
paoligg <105195009+paoligg@users.noreply.github.com>
2022-12-28 11:48:21 +0100

    Update apibot.sh

--- apibot.sh
+++ apibot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-binancedatas=$(curl -s https://rest.coinapi.io/v1/exchanges --request GET --header "X-CoinAPI-Key: A0C3673F-2BA1-432F-BA47-995C6154E45C" | grep -Poz '(?s)("BINANCE",)*n.*("KRAKEN")')
+binancedatas=$(curl -s https://rest.coinapi.io/v1/exchanges --request GET --header "X-CoinAPI-Key:<Your API Key>>" | grep -Poz '(?s)("BINANCE",)*n.*("KRAKEN")')
 volume=$(echo $binancedatas | grep -oP '(?<="volume_1hrs_usd":)[^,]*' )
 
 sqlite3 /home/ubuntu/api/BinanceVolume.db <<EOF
